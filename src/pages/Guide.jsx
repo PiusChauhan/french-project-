f19f0c02d2b292a2f473e0ce8db3b74dcd9e3c4a
Piyush Chauhan <piushhhh12@gmail.com>
2026-03-13 11:43:11 +0530

    Create Guide component for travel guide

---
+++ src/pages/Guide.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+function Guide() {
+    return (
+        <section>
+            <h1>Travel Guide</h1>
+            <p>Practical travel information and tips for France</p>
+        </section>
+    );
+}
+export default Guide;
